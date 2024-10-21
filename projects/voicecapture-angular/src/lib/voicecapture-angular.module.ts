d76b408c95e3946e8fdd528662fefa17ae95eef1
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-21 16:50:11 -0300

    adjustment export module

--- projects/voicecapture-angular/src/lib/voicecapture-angular.module.ts
+++ projects/voicecapture-angular/src/lib/voicecapture-angular.module.ts
@@ -4,5 +4,6 @@ import { VoiceCapture } from './voicecapture-angular.component';
 
 @NgModule({
   imports: [CommonModule, VoiceCapture],
+  exports: [VoiceCapture],
 })
 export class VoicecaptureModule {}
