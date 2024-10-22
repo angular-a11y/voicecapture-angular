d71defdb20811e88776a17ef5ba47ce1f1be50f8
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-10-22 01:35:30 -0300

    update example code

--- src/app/app.example.ts
+++ src/app/app.example.ts
@@ -12,25 +12,25 @@ import { VoiceCapture } from 'voicecapture-angular';
   </button>
 
   <voicecapture-angular
-    [start]="isVoiceCaptureExample"
-    (voiceTranscript)="returnVoiceTranscript($event)"
+    [start]="isVoiceState"
+    (voiceTranscript)="voiceTranscript($event)"
   />
 
   @if(voiceTextTranscript) {
     <h2>Transcript Results</h2>
-    <p class="voice-text">{{ voiceTextTranscript }}</p>
+    <p>{{ voiceTextTranscript }}</p>
   }
 \`,
 })
 export class VoiceCaptureExampleComponent {
-  isVoiceCaptureExample: WritableSignal<boolean> = signal(false);
+  isVoiceState: WritableSignal<boolean> = signal(false);
   voiceTextTranscript!: string;
 
   openVoiceCapture() {
-    this.isVoiceCaptureExample.set(true);
+    this.isVoiceState.set(true);
   }
 
-  returnVoiceTranscript(transcript: string) {
+  voiceTranscript(transcript: string) {
     this.voiceTextTranscript = transcript;
   }
 }`;
