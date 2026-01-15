358daafa1609b8f9404ff086fe4231e3e2471855
lucasferreiralimax <lucasferreiralimax@gmail.com>
2026-01-15 01:40:15 -0300

    fix: remove unnecessary options from voice activation setup

--- projects/voicecapture-angular/src/lib/voicecapture-angular.component.legacy.ts
+++ projects/voicecapture-angular/src/lib/voicecapture-angular.component.legacy.ts
@@ -41,8 +41,7 @@ export class VoiceCaptureLegacy implements OnInit {
         } else {
           this.deactivateVoice();
         }
-      },
-      { allowSignalWrites: true }
+      }
     );
   }
 
